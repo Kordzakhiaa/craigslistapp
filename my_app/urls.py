85756615a60b9b1bdc3993e83e34c0720c2d61a9
Kordzakhiaa <giorgi.kordzakhia.2@btu.edu.ge>
2021-03-01 00:01:30 +0400

    modified front also added requirements file

--- my_app/urls.py
+++ my_app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import home
+from .views import home, new_search
 
 app_name = 'my_app'
 
 urlpatterns = [
     path('', view=home, name='home_page'),
+    path('search/', view=new_search, name='new_search'),
 ]
--- my_app/views.py
+++ my_app/views.py
@@ -1,5 +1,13 @@
+import requests
 from django.shortcuts import render
+from bs4 import BeautifulSoup
 
 
 def home(request):
     return render(request, 'my_app/home.html')
+
+
+def new_search(request):
+    search = request.POST.get('search')
+    context = {'search': search}
+    return render(request, 'my_app/new_search.html', context)
