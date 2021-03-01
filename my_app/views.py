f7e01fc19026c15fc124979646a2f27aef6da365
Kordzakhiaa <giorgi.kordzakhia.2@btu.edu.ge>
2021-03-01 20:00:59 +0400

    added parsers in new_search view, modified base.html also added styles

--- my_app/views.py
+++ my_app/views.py
@@ -5,10 +5,11 @@ from urllib.parse import quote_plus
 from .models import *
 
 URL = 'https://losangeles.craigslist.org/search/?query={}'
+IMAGE_URL = ' https://www.images.craigslist.org/{}_300x300.jpg'
 
 
 def home(request):
-    return render(request, 'my_app/home.html')
+    return render(request, 'base.html')
 
 
 def new_search(request):
@@ -18,7 +19,29 @@ def new_search(request):
     response = requests.get(final_url)
     data = response.text
     soup = BeautifulSoup(data, features='html.parser')
-    post_title = soup.find_all('a', {'class': 'result-title'})
-    
-    context = {'search': search}
+    post_listings = soup.find_all('li', {'class': 'result-row'})
+
+    final_postings = []
+
+    for post in post_listings:
+        post_title = post.find(class_="result-title").text
+        post_url = post.find('a').get('href')
+        if post.find(class_="result-price"):
+            post_price = post.find(class_="result-price").text
+        else:
+            post_price = 'N/A'
+
+        if post.find(class_='result-image').get('data-ids'):
+            post_image_id = post.find(class_='result-image').get('data-ids').split(',')[0].split(':')[1]
+
+            post_image_url = IMAGE_URL.format(post_image_id)
+        else:
+            post_image_url = 'https://craigslist.org/images/peace.jpg'
+
+        final_postings.append((post_title, post_url, post_price, post_image_url))
+
+    context = {
+        'search': search,
+        'final_postings': final_postings,
+    }
     return render(request, 'my_app/new_search.html', context)
