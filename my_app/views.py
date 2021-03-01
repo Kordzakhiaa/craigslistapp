8ab4fc9ae6c41b696a8fbd5ad96c4f05da2bed78
Kordzakhiaa <giorgi.kordzakhia.2@btu.edu.ge>
2021-03-01 17:42:08 +0400

    modified navbar and started test parsing

--- my_app/views.py
+++ my_app/views.py
@@ -1,6 +1,10 @@
 import requests
 from django.shortcuts import render
 from bs4 import BeautifulSoup
+from urllib.parse import quote_plus
+from .models import *
+
+URL = 'https://losangeles.craigslist.org/search/?query={}'
 
 
 def home(request):
@@ -9,5 +13,12 @@ def home(request):
 
 def new_search(request):
     search = request.POST.get('search')
+    Search.objects.create(search=search)
+    final_url = URL.format(quote_plus(search))
+    response = requests.get(final_url)
+    data = response.text
+    soup = BeautifulSoup(data, features='html.parser')
+    post_title = soup.find_all('a', {'class': 'result-title'})
+    
     context = {'search': search}
     return render(request, 'my_app/new_search.html', context)
