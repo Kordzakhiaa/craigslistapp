08c1ff72ab04c5544a31efcd90a892039bff2d8c
Kordzakhiaa <giorgi.kordzakhia.2@btu.edu.ge>
2021-03-01 20:17:05 +0400

    added static no-image.jpg

--- my_app/views.py
+++ my_app/views.py
@@ -5,7 +5,7 @@ from urllib.parse import quote_plus
 from .models import *
 
 URL = 'https://losangeles.craigslist.org/search/?query={}'
-IMAGE_URL = ' https://www.images.craigslist.org/{}_300x300.jpg'
+IMAGE_URL = 'https://images.craigslist.org/{}_300x300.jpg'
 
 
 def home(request):
@@ -33,10 +33,10 @@ def new_search(request):
 
         if post.find(class_='result-image').get('data-ids'):
             post_image_id = post.find(class_='result-image').get('data-ids').split(',')[0].split(':')[1]
-
+            print(post_image_id)
             post_image_url = IMAGE_URL.format(post_image_id)
         else:
-            post_image_url = 'https://craigslist.org/images/peace.jpg'
+            post_image_url = 'none'
 
         final_postings.append((post_title, post_url, post_price, post_image_url))
 
